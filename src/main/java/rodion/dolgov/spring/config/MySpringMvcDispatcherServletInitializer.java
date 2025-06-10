c9580f086afb56b96578b498b2649df39ee01ce2
Dolgov <dolgovrodion@gmail.com>
2025-06-10 15:54:07 +0300

    Fixed config and dependencies in pom

--- src/main/java/rodion/dolgov/spring/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/rodion/dolgov/spring/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,12 +1,12 @@
 package rodion.dolgov.spring.config;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.Filter;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import java.nio.charset.StandardCharsets;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
