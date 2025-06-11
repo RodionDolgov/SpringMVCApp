b793bb1678d85ad8a0f3358ec612b47125834006
Dolgov <dolgovrodion@gmail.com>
2025-06-11 14:55:12 +0300

    developed CRUD in DAO

--- src/main/java/rodion/dolgov/spring/dao/PersonDAO.java
+++ src/main/java/rodion/dolgov/spring/dao/PersonDAO.java
@@ -26,27 +26,40 @@ public class PersonDAO {
         return session.createQuery("select p from Person p", Person.class).getResultList();
     }
 
+    @Transactional
     public Optional<Person> getManByEmail(String email) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("select p from Person p where p.email = :email", Person.class)
+                .setParameter("email", email).stream().findAny();
     }
 
     @Transactional
     public Person getManById(int id) {
         Session session = sessionFactory.getCurrentSession();
-        return session.createQuery("select p from Person p where p.id = :id", Person.class)
-                .setParameter("id", id)
-                .getSingleResult();
+        return session.get(Person.class, id);
     }
 
+    @Transactional
     public void save(Person person) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(person);
     }
 
+    @Transactional
     public void update(int id, Person updatedPerson) {
+        Session session = sessionFactory.getCurrentSession();
+        Person person = session.get(Person.class, id);
 
+        person.setName(updatedPerson.getName());
+        person.setAge(updatedPerson.getAge());
+        person.setEmail(updatedPerson.getEmail());
+        person.setAddress(updatedPerson.getAddress());
+        //persist
     }
 
+    @Transactional
     public void delete(int id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(session.get(Person.class, id));
     }
 }
